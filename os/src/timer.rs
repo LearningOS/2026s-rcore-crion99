3290058f3b790a4411815c3931c18b369aa47118
crion99 <3527628634@qq.com>
2026-04-10 18:04:04 +0800

    ZHOU YU

--- os/src/timer.rs
+++ os/src/timer.rs
@@ -20,15 +20,13 @@ pub fn get_time() -> usize {
 /// get current time in milliseconds
 #[allow(dead_code)]
 pub fn get_time_ms() -> usize {
-     let t = time::read() * MSEC_PER_SEC / CLOCK_FREQ;
-    if t == 0 { 1 } else { t }
+    time::read() / (CLOCK_FREQ / MSEC_PER_SEC)
 }
 
 /// get current time in microseconds
 #[allow(dead_code)]
 pub fn get_time_us() -> usize {
-   let t = time::read() * MICRO_PER_SEC / CLOCK_FREQ;
-   if t == 0 { 1 } else { t }
+    time::read() / (CLOCK_FREQ / MICRO_PER_SEC)
 }
 
 /// Set the next timer interrupt
